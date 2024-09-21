fb263266b64b141aaac6ea2df31d95c8cf649e80
carlos123asd <carlos-medin@hotmail.com>
2024-09-21 19:02:13 +0200

    fix: menu

--- src/script/components/btnNavMovile.js
+++ src/script/components/btnNavMovile.js
@@ -5,6 +5,7 @@ btnnavMovile.addEventListener('click', () => {
     const bloq1 = document.querySelector('.btnNavMovile__contents__bloq:nth-child(1)')
     const bloq2 = document.querySelector('.btnNavMovile__contents__bloq:nth-child(2)')
     const bloq3 = document.querySelector('.btnNavMovile__contents__bloq:nth-child(3)')
+    const fondo = document.getElementById('contentMain')
     const { marginBottom } = window.getComputedStyle(bloq, null);
     console.log(marginBottom)
     if(marginBottom === '6.4px'){
@@ -15,6 +16,7 @@ btnnavMovile.addEventListener('click', () => {
         bloq3.style.transform = "translate3d(0px, -7px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(-45deg) skew(0deg, 0deg)"
         bloq3.style.transformStyle = "preserve-3d"
         submenu.style.display = "block"
+        fondo.style.opacity = ".5"
     }else{
         bloq.style.marginBottom = '.4rem'
         bloq1.style.transform = "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)"
@@ -23,5 +25,6 @@ btnnavMovile.addEventListener('click', () => {
         bloq3.style.transform = "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)"
         bloq3.style.transformStyle = "preserve-3d"
         submenu.style.display = "none"
+        fondo.style.opacity = "1"
     }
 })
