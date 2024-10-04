cb74ac51d49c9ebd5ed19c7ccb6759d4a9bd0d13
carlos123asd <carlos-medin@hotmail.com>
2024-10-04 23:22:52 +0200

    Add: media query: view tablet

--- src/translate/lenguaje.js
+++ src/translate/lenguaje.js
@@ -1,18 +1,54 @@
 const translate = {
     "spanish": {
         "menu":{
-            "item1": "Inicio",
-            "item2": "Proyectos",
-            "item3": "Perfil",
-            "item4": "Contacto"
+            "item1": "INICIO",
+            "item2": "PROYECTOS",
+            "item3": "PERFIL",
+            "item4": "CONTACTO"
+        },
+        "titulos":{
+            "tit1":"Saludos",
+            "tit2":"Explora mis",
+            "tit22":"mejores proyectos", 
+            "tit3":"Un desarrollador Web Full Stack centrado en",
+            "tit33":"construir soluciones",
+            "tit4":"Desarrollador Frontend",
+            "tit5":"Desarrollador Backend",
+            "tit6":"Base de Datos",
+            "tit7":"DevOps",
+            "tit8":"Seguridad Web",
+            "tit9":"Pruebas automatizadas",
+        },
+        "parrafos":{
+            "p1":"Me apasiona la tecnología y siempre estoy abierto a nuevos Disfruto analizar problemas y encontrar soluciones prácticas que conviertan ideas en resultados tangibles. Me desenvuelvo bien en entornos colaborativos y busco agregar valor en cada etapa del Si estás buscando a alguien con una mentalidad orientada a la resolución de problemas y con disposición para aprender, estaré encantado de trabajar contigo.",
+            "p11":"desafíos.",
+            "p111":"desarrollo."
         }
     },
     "english": {
         "menu":{
-            "item1": "Home",
-            "item2": "Work",
-            "item3": "Training",
-            "item4": "Contact"
+            "item1": "HOME",
+            "item2": "WORK",
+            "item3": "TRAINING",
+            "item4": "CONTACT"
+        },
+        "title":{
+            "tit1":"Greetings",
+            "tit2":"Explore my",
+            "tit22":"best proyects",
+            "tit3":"A Full Stack web developer focused on",
+            "tit33":"building solutions",
+            "tit4":"Frontend Development",
+            "tit5":"Backend Development",
+            "tit6":"Databases",
+            "tit7":"DevOps",
+            "tit8":"Web Security",
+            "tit9":"Automated Testing",
+        },
+        "parrafos":{
+            "p1":"I’m passionate about technology and always open to new I enjoy analyzing problems and finding practical solutions that turn ideas into tangible results. I thrive in collaborative environments and aim to add value at every stage of If you're looking for someone with a problem-solving mindset and a willingness to learn, I’d be happy to work with you.",
+            "p11":"challenges.",
+            "p111":"development.",
         }
     }
 }
