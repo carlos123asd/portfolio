1aac5dc39a36044663b5724043ad6262681ed8a5
carlos123asd <carlos-medin@hotmail.com>
2024-10-05 20:53:43 +0200

    ADD: responsive deskstop

--- src/script/components/sliderProyectPhotos.js
+++ src/script/components/sliderProyectPhotos.js
@@ -2,7 +2,12 @@ const swiperPhotosProyect = new Swiper('.swiperPhotosProyect', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+    spaceslidesPerView: 1,
+    breakpoints: {
+      1008: {
+        spaceBetween: 400
+      }
+    },
     scrollbar: {
       el: '.swiper-scrollbar',
     }
